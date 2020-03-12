a7d7b7f480d950ff60a271625341808f82175674
Jair Jaramillo <jair.jaramillo@outlook.com>
2020-03-11 21:06:09 -0600

    Add main function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import './style.css';
 import Background from './pic/bg.jpg';
 
-// eslint-disable-next-line no-console
-console.log('Hello world');
-const bg = new Image();
-bg.src = Background;
+const main = () => {
+  const bg = new Image();
+  bg.src = Background;
+
+  const jumbo = document.createElement('div');
+  jumbo.class = 'jumbotron text-center bg';
+  jumbo.innerHTML = `
+    <h1>Cafe Plaza</h1>
+    <p>Cafe - Food - Ambient</p>
+  `;
+
+  return jumbo;
+};
+
+document.getElementById('content').appendChild(main());
